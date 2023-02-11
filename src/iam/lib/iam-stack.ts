7c42899fe9e910c0189e0605b28c7fe9acc9713c
tamakiii <tamakiii@users.noreply.github.com>
2023-02-12 01:30:26 +0900

    add HelloRole

--- src/iam/lib/iam-stack.ts
+++ src/iam/lib/iam-stack.ts
@@ -1,12 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 
 export class IamStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    const role = new Role(this, 'HelloRole', {
+      assumedBy: new ServicePrincipal('sns.amazonaws.com'),
+    });
 
     // example resource
     // const queue = new sqs.Queue(this, 'IamQueue', {
